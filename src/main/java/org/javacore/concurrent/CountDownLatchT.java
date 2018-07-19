473b215de87af4190fb6dc3651fac991d8a75e10
huhaojian <huhaojian@jd.com>
2018-07-19 19:04:52 +0800

    improve

--- src/main/java/org/javacore/concurrent/CountDownLatchT.java
+++ src/main/java/org/javacore/concurrent/CountDownLatchT.java
@@ -13,11 +13,15 @@ import java.util.concurrent.TimeUnit;
  */
 public class CountDownLatchT {
 
-    // 线程中止的计数器
+    /**
+     * 线程中止的计数器
+     */
     private final static int COUNT = 10;
     private final static CountDownLatch count = new CountDownLatch(COUNT);
 
-    // 线程池
+    /**
+     * 线程池
+     */
     private final static ExecutorService service = Executors.newFixedThreadPool(5);
 
     public static void main(String[] args) throws InterruptedException {
@@ -27,10 +31,13 @@ public class CountDownLatchT {
                     int time = new Random().nextInt(5);
                     TimeUnit.SECONDS.sleep(time);
                     System.out.printf("Thread %s ## 耗时:%d\n", Thread.currentThread().getId(), time);
-                    // 线程结束后,计数器减一
-                    count.countDown();
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }
+                finally {
+                    // 线程结束后,计数器减一
+                    count.countDown();
                 }
 
             });
--- src/main/java/org/javacore/initAndCleanup/SimpleConstructor2.java
+++ src/main/java/org/javacore/initAndCleanup/SimpleConstructor2.java
@@ -5,9 +5,9 @@ package org.javacore.initAndCleanup;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,9 +28,11 @@ public class SimpleConstructor2 {
 	}
 }
 
-// Child类
+/**
+ * Child类
+ */
 class Child2 {
 	Child2(int i) {// 带参数的Child类构造器
-		System.out.print("Child init " + i + " ");
+		System.out.println("Child init " + i + " ");
 	}
 }
